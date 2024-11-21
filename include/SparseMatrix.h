bfb99bf1248da6b62fdc932b011737245ce16bf6
Luis Gomez-Acebo <124214330+Fruitkeeper@users.noreply.github.com>
2024-11-21 12:42:15 +0100

    Update and rename sparse_matrix.h to SparseMatrix.h

---
+++ include/SparseMatrix.h
@@ -0,0 +1,23 @@
+#ifndef SPARSEMATRIX_H
+#define SPARSEMATRIX_H
+
+#include <vector>
+#include <string>
+#include <utility>
+
+namespace sparsematrix {
+
+class SparseMatrix {
+public:
+    virtual ~SparseMatrix() = default;
+
+    virtual SparseMatrix* convertTo(const std::string& format) const = 0;
+    virtual void toDense(std::vector<std::vector<double>>& denseMatrix) const = 0;
+    virtual void transpose() = 0;
+    virtual std::pair<int, int> getShape() const = 0;
+    virtual int getNNZ() const = 0;
+};
+
+} // namespace sparsematrix
+
+#endif // SPARSEMATRIX_H
