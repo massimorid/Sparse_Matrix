383d06d308ed4175dcec9d995db426773eb4b0ed
Luis Gomez-Acebo <124214330+Fruitkeeper@users.noreply.github.com>
2024-11-21 13:06:44 +0100

    Update and rename sparse_matrix.cpp to SparseMatrix.cpp

---
+++ src/SparseMatrix.cpp
@@ -0,0 +1,24 @@
+#include "SparseMatrix.h"
+#include "CSRMatrix.h"
+#include "CSCMatrix.h"
+#include "COOMatrix.h"
+#include "DiagonalMatrix.h"
+#include <stdexcept>
+
+namespace sparsematrix {
+
+SparseMatrix* SparseMatrix::fromDense(const std::vector<std::vector<double>>& denseMatrix, const std::string& format) {
+    if (format == "CSR") {
+        return new CSRMatrix(denseMatrix);
+    } else if (format == "CSC") {
+        return new CSCMatrix(denseMatrix);
+    } else if (format == "COO") {
+        return new COOMatrix(denseMatrix);
+    } else if (format == "Diagonal") {
+        return new DiagonalMatrix(denseMatrix);
+    } else {
+        throw std::invalid_argument("Unsupported format: " + format);
+    }
+}
+
+} // namespace sparsematrix
