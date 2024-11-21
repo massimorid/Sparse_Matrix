caf983264c08a3e9797805e59f4a5eb1aaa78c94
Luis Gomez-Acebo <124214330+Fruitkeeper@users.noreply.github.com>
2024-11-21 12:47:32 +0100

    Create CSCMatrix.h

---
+++ include/CSCMatrix.h
@@ -0,0 +1,28 @@
+#ifndef CSCMATRIX_H
+#define CSCMATRIX_H
+
+#include "SparseMatrix.h"
+
+namespace sparsematrix {
+
+class CSCMatrix : public SparseMatrix {
+private:
+    std::vector<double> values;
+    std::vector<int> rowIndices;
+    std::vector<int> colPointers;
+    int rows, cols;
+
+public:
+    CSCMatrix(const std::vector<double>& values, const std::vector<int>& rowIndices,
+              const std::vector<int>& colPointers, int rows, int cols);
+
+    SparseMatrix* convertTo(const std::string& format) const override;
+    void toDense(std::vector<std::vector<double>>& denseMatrix) const override;
+    void transpose() override;
+    std::pair<int, int> getShape() const override;
+    int getNNZ() const override;
+};
+
+} // namespace sparsematrix
+
+#endif // CSCMATRIX_H
